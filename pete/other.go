81639152f4b72c2a7c3fdd3d9600ef53710f03ba
Pete Moore <pmoore@mozilla.com>
2015-01-14 18:10:22 +0100

    stuff

--- pete/other.go
+++ pete/other.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+//    "github.com/petemoore/go_tutorial/lib"
 )
 
 type fred struct {
@@ -15,7 +16,36 @@ type tom struct {
 }
 
 func main() {
-    fmt.Println("Hello")
-    f := tom{j: 3, f: fred{x: "yo", y: 5}}
-    fmt.Println(f.f.x)
+    t := tom{j: 3, f: fred{x: "yo", y: 5}}
+    fmt.Println("Pointer/Value differences w.r.t. if function is defined against pointer or value")
+    fmt.Println("================================================================================")
+    fmt.Printf("Initial value: %v\n", t.j)
+    t.no_play()
+    fmt.Printf("When function is defined against a value: %v\n", t.j)
+    t.play_with()
+    fmt.Printf("When function is defined against a pointer: %v\n", t.j)
+
+    fmt.Println("Pointer/Value differences when passed as a parameter to a function")
+    fmt.Println("==================================================================")
+    fmt.Printf("Initial value: %v\n", t.j)
+    no_fiddle(t)
+    fmt.Printf("When function is passed a value: %v\n", t.j)
+    fiddle(&t)
+    fmt.Printf("When function is passed a pointer: %v\n", t.j)
+}
+
+func no_fiddle(t tom) {
+    t.j = 15
+}
+
+func fiddle(t *tom) {
+    t.j = 15
+}
+
+func (t *tom) play_with() {
+    t.j = 12
+}
+
+func (t tom) no_play() {
+    t.j = 12
 }
