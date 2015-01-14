c9db8062633d95de873d2ea913240f2195e61b40
Pete Moore <pmoore@mozilla.com>
2015-01-14 14:19:05 +0100

    stuff

--- pete/other.go
+++ pete/other.go
@@ -4,6 +4,18 @@ import (
     "fmt"
 )
 
+type fred struct {
+    x string
+    y int
+}
+
+type tom struct {
+    f fred
+    j uint8
+}
+
 func main() {
     fmt.Println("Hello")
+    f := tom{j: 3, f: fred{x: "yo", y: 5}}
+    fmt.Println(f.f.x)
 }
